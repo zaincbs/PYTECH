2c067405ec10161179fe80485ffa3e12465f5c01
abdulsamad786 <abdulsamadabdulrab@gmail.com>
2016-08-30 13:46:38 -0700

    computing-the-length-of-input-paramter

---
+++ compute-length.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python      
+
+
+"""
+Define a function that computes the length of a given list or string.
+
+"""
+
+def lengthOfStrOrList(input):
+	return len(input)
+
+def main():
+	s = lengthOfStrOrList(("input"))
+	print "The length is", s
+if __name__ == "__main__":
+    main()
+
