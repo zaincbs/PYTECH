9a60b8bab595147d62bc029eb8a56422f7417051
abdulsamad786 <abdulsamadabdulrab@gmail.com>
2016-09-18 15:03:42 -0700

    modifying 99_Bottles_of_Beer.py program

--- 99_Bottles_of_Beer.py
+++ 99_Bottles_of_Beer.py
@@ -23,7 +23,10 @@ def bottles_decrementor(n):
     for i in range(n,1,-1):
         bottles= bottles -1 
         bottle = bottle-1
-        print bottles , "bottles of beer on the wall, ", bottles, "bottles of beer.\nTake one down, pass it around, " , bottle, " bottles of beer on the wall." 
+        if bottle == 0:
+            print bottles , "bottles of beer on the wall, ", bottles, " bottles of beer.\nTake one down, pass it around, " , bottle, " bottle of beer on the wall."
+        else:
+            print bottles , "bottles of beer on the wall, ", bottles, "bottles of beer.\nTake one down, pass it around, " , bottle, " bottles of beer on the wall." 
 
 def main():
     v= bottles_decrementor(100)
