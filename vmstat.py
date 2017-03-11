e30ca995c88162daced7d7661493d1cf56e037f7
abdulsamad786 <abdulsamadabdulrab@gmail.com>
2017-03-11 00:17:56 +0000

    new updates on vmstat.py

--- vmstat.py
+++ vmstat.py
@@ -11,6 +11,7 @@ import itertools
 def vmstat():
     myList=[]    
     newList=[]
+    saveDataList=[]
 
     fileobj = open('/proc/meminfo','r')
     fileReader = fileobj.readlines() 
@@ -26,27 +27,42 @@ def vmstat():
     #converting topNlines to a list, since itertools.islice return
     #an object
     myList= list(topNlines)
-    print myList
+    #print myList
     
     #removing spaces from list elements and creating a newList so that
     #it is converted further into tokens
     for i in myList:
         i = i.split()
         newList.append(i)
-
-    #[['MemTotal:', '1017564', 'kB'], ['MemFree:', '108396', 'kB'], ['Buffers:', '58764', 'kB'], ['Cached:', '219060', 'kB']]
-
+    """
+    newList = [['MemTotal:', '1017564', 'kB'], ['MemFree:', '108396', 'kB'], ['Buffers:', '58764', 'kB'], ['Cached:', '219060', 'kB']]
+    """
     #calculations of memmory: free_cached, free_unused, used
     memTotal = newList[0][1]
     free_cached = int(newList[2][1] + newList[3][1])
     free_unused = int(newList[1][1])
     used = int(newList[0][1]) - free_cached - free_cached
 
-    
+    saveDataList.append(['system.mem.free_cached ',' ', free_cached , ' ',memTotal])
+    saveDataList.append(['system.mem.free_unused ' ,newList[0][1], ' ',memTotal ])
+    saveDataList.append(['system.mem.used  ', used, ' ' ,memTotal ])
+    #print saveDataList
+    #print ""
+
+    #for data_stored in range(0, len(saveDataList)-1):
+    #    print saveDataList[data_stored]
+
     print 'system.mem.free_cached ',' ', free_cached , ' ',memTotal
     print 'system.mem.free_unused ' ,newList[0][1], ' ',memTotal
     print 'system.mem.used  ', used, ' ' ,memTotal
 
+"""
+$ ./vmstat.py
+system.mem.free_cached    62756208932   1017564
+system.mem.free_unused  1017564   1017564
+system.mem.used   -125511400300   1017564 
+
+"""
 
 def main():
     v = vmstat()
