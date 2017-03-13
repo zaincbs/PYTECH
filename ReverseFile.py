8540c955c96c065c3799db9e047fafbc9a062bf8
abdulsamad786 <abdulsamadabdulrab@gmail.com>
2017-03-13 23:18:41 +0000

    Reversing the text in the file

---
+++ ReverseFile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+
+Reversing the text in a file
+"""
+import sys 
+
+
+def reverseFile(filename):
+    f = open(filename,'r')
+    for line in f:
+        line = line[::-1]
+        #list.append( line )
+	print line
+    f.close()
+
+
+def main():
+    argList= sys.argv
+    file_to_be_read = argList[1]
+    v = reverseFile(file_to_be_read)
+                                                            
+if __name__ == "__main__":
+    main()
+
+"""
+$ ./ReverseFile.py feed.text
+
+god yzal eht revo spmuj xof nworb kciuq ehT 
+
+"""
